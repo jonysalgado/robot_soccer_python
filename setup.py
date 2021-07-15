807ccf912ac7794e141bf3b55b46d44ddc442e86
Jony Salgado <50979367+jonysalgado@users.noreply.github.com>
2021-07-15 12:05:45 -0300

    up version 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'robot_soccer_python',         # How you named your package folder (MyLib)
   packages = ['robot_soccer_python'],   # Chose the same as "name"
-  version = '1.0.0',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A robot soccer simulation 2D environment for python.',   # Give a short description about your library
   author = 'Jony Salgado',                   # Type in your name
@@ -12,8 +12,7 @@ setup(
   keywords = ['SIMULATION', 'SOCCER', 'ROBOT', 'AI', 'ENVIRONMENT', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         'numpy',
-        'pygame',
-        'math'
+        'pygame'
      ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
