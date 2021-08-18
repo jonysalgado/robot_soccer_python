441b6ac7b617da6658f5aa356a5c050b7942d22a
jonysalgado <jonysalgadofilho@gmail.com>
2021-08-17 21:05:06 -0300

    up version files

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   author = 'Jony Salgado',                   # Type in your name
   author_email = 'jonysalgadofilho@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jonysalgado/robot_soccer_python',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jonysalgado/robot_soccer_python/archive/refs/tags/v_01.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jonysalgado/robot_soccer_python/archive/refs/tags/v_01.5.tar.gz',    # I explain this later on
   keywords = ['SIMULATION', 'SOCCER', 'ROBOT', 'AI', 'ENVIRONMENT', 'PYTHON'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         'numpy',
